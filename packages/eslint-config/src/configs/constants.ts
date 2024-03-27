ba2594074a20dbf31918d6946290174772556a72
Fernando Isidro Luna <luffynando@gmail.com>
2024-03-26 22:59:42 -0600

    fix(eslint): :bug: resolve file extension vue as allowed and refactor ignores

--- packages/eslint-config/src/configs/constants.ts
+++ packages/eslint-config/src/configs/constants.ts
@@ -4,7 +4,7 @@ export const allJsExtensions = 'js,mjs,cjs,ts,mts,cts';
 
 export const supportedFileTypes = `**/*.{${allJsExtensions}}`;
 
-export const allFilesSupported = `**/*.{${allJsExtensions},d.ts.vue}`;
+export const allFilesSupported = `**/*.{${allJsExtensions},d.ts,vue}`;
 
 export const ignores = [
   '**/node_modules/**',
@@ -15,14 +15,15 @@ export const ignores = [
   '**/docs/**',
   '**/temp/**',
   '**/vendor/**',
-  '**/eslint.config.{js,mjs,cjs,cts}', // we currently cannot lint the eslint.config.js itself. It is currently only provided as a .js file and this config currently only supports .ts files. Therefore, eslint.config.js can only be re-enabled once this config support pure .js files too, or the ESLint team support the eslint.config.ts file.
-  '**/prettier.config.{js,mjs,cjs,cts}',
-  '**/commitlint.config.{js,mjs,cjs,cts}',
+  '**/public/**',
+  `**/eslint.config.{${allJsExtensions}}`, // we currently cannot lint the eslint.config.js itself. It is currently only provided as a .js file and this config currently only supports .ts files. Therefore, eslint.config.js can only be re-enabled once this config support pure .js files too, or the ESLint team support the eslint.config.ts file.
+  `**/prettier.config.{${allJsExtensions}}`,
+  `**/commitlint.config.{${allJsExtensions}}`,
+  '**/package-lock.json',
   '**/pnpm-lock.yaml',
   '**/yarn.lock',
   '**/composer.lock',
   '**/composer.json',
   '**/package.json',
-  '**/package-lock.json',
   '**/*.min.*',
 ];
