2a51d2881dfb79031fcefbfd893f0b7d78ad0cd3
Fernando Isidro Luna <luffynando@gmail.com>
2024-08-16 12:37:37 -0600

    chore: :arrow_up: update dependencies and configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 // @ts-check
+import { getDirname } from '@poppinss/utils';
 import { nodecfdiConfig } from '@nodecfdi/eslint-config';
 import { defineFlatConfig } from 'eslint-define-config';
 
-export default defineFlatConfig([...nodecfdiConfig()]);
+export default defineFlatConfig([
+  ...nodecfdiConfig({ experimentalProjectService: getDirname(import.meta.url) }),
+]);
